d6b5287da5b6cae80dc2b1a421e44c8fd127c73a
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2025-08-20 09:49:21 +0900

    Test API

--- api/products.ts
+++ api/products.ts
@@ -1,20 +1,21 @@
-import mysql from "mysql2/promise";
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+import mysql from "mysql2/promise"
 
-export default async function products(req, res) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const connection = await mysql.createConnection({
       host: process.env.MYSQL_HOST,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
-    });
+    })
 
-    const [rows] = await connection.execute("SELECT * FROM products");
-    await connection.end();
+    const [rows] = await connection.execute("SELECT * FROM products")
+    await connection.end()
 
-    res.status(200).json(rows);
+    res.status(200).json(rows)
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error(error)
+    res.status(500).json({ error: "Internal Server Error" })
   }
 }
