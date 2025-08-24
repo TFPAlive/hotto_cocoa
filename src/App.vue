c3d54b776903712ed8a20494b43b3288445c4f9a
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2025-08-24 13:05:37 +0900

    Test Speed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import SideMenu from './components/layout/SideMenu.vue'
 import FooterMenu from './components/layout/FooterMenu.vue'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
   <SideMenu />
-
   <main>
     <RouterView />
   </main>
-
   <FooterMenu />
+
+  <SpeedInsights />
 </template>
