903859b374e3b6fb0f323887daab78cdab9d7629
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2025-08-24 16:07:01 +0900

    Fix pathing issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import MainLayout from "../layouts/MainLayout.vue";
-import AdminLayout from "../layouts/AdminLayout.vue";
-import Landing from "../components/admin/Landing.vue";
-import Home from "../components/views/Home.vue";
+import MainLayout from "@/layouts/MainLayout.vue";
+import AdminLayout from "@/layouts/AdminLayout.vue";
+import Landing from "@/components/admin/Landing.vue";
+import Home from "@/components/views/Home.vue";
+import Design from "@/components/views/Design.vue";
+import Login from "@/components/auth/Login.vue";
+import Register from "@/components/auth/Register.vue";
+
 
 const routes = [
   {
     path: "/",
     component: MainLayout,
     children: [
-      { path: "", component: Home },
+      { path: "/", component: Home },
+      { path: "/design", component: Design },
+      { path: "/login", component: Login },
+      { path: "/register", component: Register },
       // ...other normal pages
     ]
   },
