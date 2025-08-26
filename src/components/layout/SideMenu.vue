42c6195f6b802b49e0cbdabfa2100ece72951e99
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2025-08-27 02:06:12 +0900

    Test

--- src/components/layout/SideMenu.vue
+++ src/components/layout/SideMenu.vue
@@ -8,6 +8,9 @@ const auth = useAuthStore()
 const router = useRouter()
 
 function handleLogout() {
+  fetch("/api/auth/logout", { method: "POST" }) // optional
+  auth.logout()
+  router.push("/")
 }
 </script>
 
@@ -22,14 +25,7 @@ function handleLogout() {
     <Navigation />
     <div class="navbar-right">
       <CartMenu />
-      <div v-if="auth.isLoggedIn">
-        <button class="login-btn" @click="handleLogout">Logout</button>
-      </div>
-      <div v-else>
-        <router-link to="/login">
-          <button class="login-btn">Login</button>
-        </router-link>
-      </div>
+      
     </div>
   </header>
 </template>
