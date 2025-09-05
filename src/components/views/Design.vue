70a6d80bce5622b434cd684bbade59cc0fea3413
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2025-09-05 11:16:35 +0900

    Test Chatbot

--- src/components/views/Design.vue
+++ src/components/views/Design.vue
@@ -3,7 +3,6 @@ import Banner from '../home/Banner.vue'
 import Welcome from '../design/Welcome.vue'
 import ChatBox from '../design/ChatBox.vue'
 import DesignCorner from '../design/DesignCorner.vue';
-import Test from '../design/Test.vue';
 import { onMounted } from 'vue'
 
 onMounted(() => {
@@ -14,7 +13,6 @@ onMounted(() => {
 <template>
   <Welcome />
   <Banner />
-  <Test />
   <ChatBox />
   <DesignCorner />
 </template>
