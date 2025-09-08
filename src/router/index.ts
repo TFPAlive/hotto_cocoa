7158971cfe94e9153ef66340b6048e7f42450b49
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2025-09-08 13:35:46 +0900

    Fix scroll behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,4 +33,16 @@ const routes = [
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { 
+        el: to.hash,
+        behavior: 'smooth',
+      };
+    }
+    return { top: 0 };
+  }
 });
